810a7f977e830c8da8b929497df9116c3c635e82
okoh-peter <okohpeter13@gmail.com>
2025-06-21 14:56:58 +0100

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+  
+  // Load the Google Analytics script asynchronously
+(function() {
+  var gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-11383063131';
+  document.head.appendChild(gaScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag; // expose globally
+
+  gtag('js', new Date());
+  gtag('config', 'G-Y906QGSN42');
+})();
+  
+  
+  
   const menuIcon = document.getElementById('menu-icon');
   const menu = document.getElementById('menu-toggle');
 
@@ -5,5 +23,7 @@
     menu.classList.toggle('show');
     menuIcon.textContent = menu.classList.contains('show') ? '✖' : '☰';
   });
+
+
 
 
