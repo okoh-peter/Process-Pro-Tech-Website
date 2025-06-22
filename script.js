bb5c213665eccab638b1677423ef652c39a03104
okoh-peter <okohpeter13@gmail.com>
2025-06-22 14:40:19 +0100

    Remove duplicate GA script from script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-  
-  // Load the Google Analytics script asynchronously
-(function() {
-  var gaScript = document.createElement('script');
-  gaScript.async = true;
-  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-Y906QGSN42';
-  document.head.appendChild(gaScript);
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  window.gtag = gtag; // expose globally
-
-  gtag('js', new Date());
-  gtag('config', 'G-Y906QGSN42');
-})();
-  
-  
-  
-  const menuIcon = document.getElementById('menu-icon');
+ const menuIcon = document.getElementById('menu-icon');
   const menu = document.getElementById('menu-toggle');
 
   menuIcon.addEventListener('click', () => {
